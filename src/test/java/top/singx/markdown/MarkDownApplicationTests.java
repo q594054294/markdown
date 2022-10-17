6fcd1ac709eaeadc1b2a0229ce88893d679eef38
xuchang <xu940803@gmail.com>
2022-10-17 16:02:40 +0800

    首次提交

---
+++ src/test/java/top/singx/markdown/MarkDownApplicationTests.java
@@ -0,0 +1,13 @@
+package top.singx.markdown;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarkDownApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
